c6b1506529890d81385c1b66d768e0024e5a1798
mihu19 <mihai.botezat19@gmail.com>
2026-09-11 12:41:44 +0300

    moved documentation inside user controller, deleted redundant info

--- src/main/kotlin/com/example/demo/config/SwaggerConfig.kt
+++ src/main/kotlin/com/example/demo/config/SwaggerConfig.kt
@@ -2,13 +2,10 @@ package com.example.demo.config
 
 import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
-import io.swagger.v3.oas.models.info.Contact
 import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.responses.ApiResponse
 import io.swagger.v3.oas.models.security.SecurityRequirement
 import io.swagger.v3.oas.models.security.SecurityScheme
-import io.swagger.v3.oas.models.servers.Server
-import io.swagger.v3.oas.models.tags.Tag
 import jakarta.annotation.PostConstruct
 import org.springdoc.core.customizers.OpenApiCustomizer
 import org.springdoc.core.properties.SwaggerUiConfigProperties
@@ -53,17 +50,6 @@ class SwaggerConfig(
                         """.trimIndent()
                     )
             )
-            .servers(
-                listOf(
-                    Server().url("http://localhost:8081").description("Local Development"),
-                    Server().url("https://volunteer-be-rs60.onrender.com/").description("Render Server")
-                )
-            )
-            .tags(
-                listOf(
-                    Tag().name("Users").description("Operations related to user accounts and profiles")
-                )
-            )
             .components(
                 Components()
                     .addSecuritySchemes(
@@ -86,50 +72,5 @@ class SwaggerConfig(
             )
             .addSecurityItem(SecurityRequirement().addList(BEARER_AUTH))
             .addSecurityItem(SecurityRequirement().addList(BASIC_AUTH))
-    }
-
-    @Bean
-    fun openApiCustomizer(): OpenApiCustomizer {
-        return OpenApiCustomizer { openApi ->
-
-            openApi.paths["/api/v1/users"]?.get?.apply {
-                tags = listOf("Users")
-                summary = "Retrieve all users"
-                description = "Fetches a full list of all registered users in the database."
-                responses.addApiResponse("200", ApiResponse().description("Users retrieved successfully"))
-                responses.addApiResponse("401", ApiResponse().description("Unauthorized access"))
-            }
-
-            openApi.paths["/api/v1/users/{id}"]?.get?.apply {
-                tags = listOf("Users")
-                summary = "Get user by ID"
-                description = "Finds a single user profile using their MongoDB ObjectId."
-                parameters?.find { it.name == "id" }?.apply {
-                    description = "Hexadecimal string representing the user ID"
-                    example = "64f1a2b3c4d5e6f7a8b9c0d1"
-                }
-
-                //to change after exception handling implemented
-                responses.addApiResponse("200", ApiResponse().description("User found"))
-                responses.addApiResponse("404", ApiResponse().description("User not found"))
-                responses.addApiResponse("401", ApiResponse().description("Unauthorized access"))
-            }
-
-            openApi.components?.schemas?.get("User")?.apply {
-                description = "Data transfer object representing a user"
-                properties?.get("id")?.apply {
-                    description = "Unique MongoDB ObjectId"
-                    example = "64f1a2b3c4d5e6f7a8b9c0d1"
-                }
-                properties?.get("name")?.apply {
-                    description = "User's full name"
-                    example = "Kebab Nurmagomedov"
-                }
-                properties?.get("age")?.apply {
-                    description = "User's age in years"
-                    example = 28
-                }
-            }
-        }
     }
 }
